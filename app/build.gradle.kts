33d24d60ec0bb8dd217ff4ba9b521c66b15411d1
Sora0 <sokkuru0924@docomo.ne.jp>
2025-06-18 13:01:46 +0900

    6/18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,13 +32,14 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation ("com.mihir.drawingcanvas:drawingcanvas:1.0.1")
 }
